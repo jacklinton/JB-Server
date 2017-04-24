dbe33811c7b142000cbfcafff59a7268fbe6d061
Jack Linton <jacklinton@mac.com>
2017-04-23 23:33:41 -0400

    Modified several files including the connectors file which now has a rough outline of the eventual content. The primary goal now is to finalize the schams and models and on the mongoose framework so that the metadata from archive.org can be transfered en masse into the local DB in an organized and easily acccessible way. From there, the graphql schemas and resolvers need to be finalized and tested. The last step for creating the database at this stage is to create an authentication system which includes OAuth with facebook at least and hopefully google and twitter as well. Then work on the frontend can begin.

---
+++ connectors.js
@@ -0,0 +1,15 @@
+import Mongoose from 'mongoose';
+
+const mongo = Mongoose.connect('mongodb://localhost/data/db');
+
+const SongSchema = Mongoose.schema({
+
+});
+
+const ShowShema = Mongoose.schema({
+
+});
+
+const ArtistSchema = Mongoose.schema({
+
+});

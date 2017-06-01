ac1bdb588a7ed3a164b78af75d08aecf1a062535
Jack Linton <user01@Jacks-MacBook-Pro.local>
2017-06-01 19:31:31 -0400

    Updated to node 8 npm 5 and installed nvm.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: ['./lib/components/index.js'],
+    entry: ['babel-polyfill', './lib/components/index.js'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
